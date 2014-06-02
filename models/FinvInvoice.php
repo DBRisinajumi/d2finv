79b555c44f684031f2b13792d9598f717c150536
ivarsju <ivars.ju@gmail.com>
2014-06-03 01:49:04 +0300

    added recalcInvoice

--- models/FinvInvoice.php
+++ models/FinvInvoice.php
@@ -66,5 +66,56 @@ class FinvInvoice extends BaseFinvInvoice
         return parent::save($runValidation, $attributes);
 
     }
+    
+    /**
+     * recalc totals
+     */
+    public function recalcInvoice($finv_id) {
+        $this->unsetAttributes();
+        $this->finv_id = $finv_id;
+        $this->refresh();
+        $this->finv_amt = 0;
+        $this->finv_total = 0;
+        $this->finv_vat = 0;
+
+        $criteria = new CDbCriteria;
+        $criteria->compare('t.fiit_finv_id', $finv_id);
+        //$fiit_model = new FiitInvoiceItem();
+        //$fiit_model->setAttribute('fiit_finv_id', $finv_id);
+        $fiits = FiitInvoiceItem::model()->findAll($criteria);
+        foreach ($fiits as $fiit) {
+            $fiit->fiit_amt = round($fiit->fiit_price * $fiit->fiit_quantity, 2);
+            $fiit->fiit_total = $fiit->fiit_amt;
+            $fiit->fiit_vat = 0;
+            try {
+                if (!$fiit->save()) {
+                    $this->addError('fiit_id', 'Can not update fiit item');
+                    return FALSE;
+                }
+            } catch (Exception $e) {
+                $this->addError('', $e->getMessage());
+                return FALSE;
+            }
+
+            $this->finv_amt += $fiit->fiit_amt;
+            $this->finv_total += $fiit->fiit_total;
+            $this->finv_vat += $fiit->fiit_vat;
+        }
+
+        $this->finv_basic_amt = Yii::app()->currency->convertToBase($this->finv_amt, $this->finv_fcrn_id, $this->finv_date);
+        $this->finv_basic_total = Yii::app()->currency->convertToBase($this->finv_total, $this->finv_fcrn_id, $this->finv_date);
+        $this->finv_basic_vat = Yii::app()->currency->convertToBase($this->finv_vat, $this->finv_fcrn_id, $this->finv_date);
+        try {
+            if (!$this->save()) {
+                $this->addError('finv_id', 'Can not update finv record');
+                return FALSE;
+            }
+        } catch (Exception $e) {
+            $this->addError('finv_id', $e->getMessage());
+            return FALSE;
+        }
+
+        return TRUE;
+    }
 
 }
