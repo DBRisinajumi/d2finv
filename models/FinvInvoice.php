9ebef4ef81b79fd3ad231f8a680e66e0f1c3a9fa
ivarsju <ivars.ju@gmail.com>
2014-06-05 03:07:37 +0300

    added getTotals, beforeFind, beForeDelete, save and other methods

--- models/FinvInvoice.php
+++ models/FinvInvoice.php
@@ -6,6 +6,8 @@ Yii::import('FinvInvoice.*');
 
 class FinvInvoice extends BaseFinvInvoice
 {
+    
+    public $finv_date_range;
 
     // Add your model-specific methods here. This file will not be overriden by gtc except you force it.
     public static function model($className = __CLASS__)
@@ -15,6 +17,11 @@ class FinvInvoice extends BaseFinvInvoice
 
     public function init()
     {
+        // Default values
+        $this->finv_fcrn_id     = Yii::app()->sysCompany->getAttribute('base_fcrn_id');
+        $this->finv_date        = date('Y-m-d');
+        $this->finv_budget_date = date('Y-m-d');
+        
         return parent::init();
     }
 
@@ -34,11 +41,13 @@ class FinvInvoice extends BaseFinvInvoice
     public function rules()
     {
         return array_merge(
-            parent::rules()
-        /* , array(
-          array('column1, column2', 'rule1'),
-          array('column3', 'rule2'),
-          ) */
+            parent::rules(),
+            array(
+                array('finv_sys_ccmp_id, finv_basic_fcrn_id', 'required', 'message'=>Yii::t('D2finvModule.model', 'System configuration error \'{attribute}\' is not set.')),
+                array('finv_sys_ccmp_id', 'default', 'value'=>Yii::app()->sysCompany->getActiveCompany()),
+                array('finv_basic_fcrn_id', 'default', 'value'=>Yii::app()->sysCompany->getAttribute('base_fcrn_id')),
+                array('finv_reg_date', 'default', 'value'=>date('Y-m-d')),
+            )
         );
     }
 
@@ -55,41 +64,40 @@ class FinvInvoice extends BaseFinvInvoice
     public function save($runValidation = true, $attributes = NULL) 
     {
         
-        // set system company id
-        if ($this->isNewRecord && Yii::app()->sysCompany->getActiveCompany()) {
-            $this->finv_sys_ccmp_id   = Yii::app()->sysCompany->getActiveCompany();
-            $this->finv_fcrn_id       = Yii::app()->sysCompany->getAttribute('base_fcrn_id');
-            $this->finv_basic_fcrn_id = Yii::app()->sysCompany->getAttribute('base_fcrn_id');
-            $this->finv_reg_date      = date('Y-m-d H:i:s');
+        if ($this->isNewRecord) {
+            parent::save($runValidation, $attributes);
         }
         
-        return parent::save($runValidation, $attributes);
-
-    }
-    
-    /**
-     * recalc totals
-     */
-    public function recalcInvoice($finv_id) {
-        $this->unsetAttributes();
-        $this->finv_id = $finv_id;
-        $this->refresh();
-        $this->finv_amt = 0;
+        $finv_id = $this->finv_id;
+        
+        //$this->unsetAttributes();
+        //$this->refresh();
+        $this->finv_amt   = 0;
         $this->finv_total = 0;
-        $this->finv_vat = 0;
+        $this->finv_vat   = 0;
 
         $criteria = new CDbCriteria;
         $criteria->compare('t.fiit_finv_id', $finv_id);
-        //$fiit_model = new FiitInvoiceItem();
-        //$fiit_model->setAttribute('fiit_finv_id', $finv_id);
         $fiits = FiitInvoiceItem::model()->findAll($criteria);
+        
         foreach ($fiits as $fiit) {
+            
+            $criteria = new CDbCriteria;
+            $criteria->compare('t.fvat_id', $fiit->fiit_fvat_id);
+            $fvat = FvatVat::model()->findAll($criteria);
+            
+            $fvat_rate = $fvat[0]->fvat_rate;
+            
             $fiit->fiit_amt = round($fiit->fiit_price * $fiit->fiit_quantity, 2);
-            $fiit->fiit_total = $fiit->fiit_amt;
-            $fiit->fiit_vat = 0;
+            $fiit->fiit_vat = round($fiit->fiit_amt * $fvat_rate / 100, 2);
+            // varbūt šādi? atšķiras apaļošana
+            //$fiit->fiit_total = round($fiit->fiit_amt * (1 + $fvat_rate), 2);
+            $fiit->fiit_total = $fiit->fiit_amt + $fiit->fiit_vat;
+            
             try {
-                if (!$fiit->save()) {
-                    $this->addError('fiit_id', 'Can not update fiit item');
+                // don't use $fiit->save() there it will run infinite loop
+                if (!$fiit->saveWithoutRecalc()) {
+                    $this->addError('fiit_id', Yii::t('D2finvModule.model', 'Can not update invoice item!'));
                     return FALSE;
                 }
             } catch (Exception $e) {
@@ -97,25 +105,111 @@ class FinvInvoice extends BaseFinvInvoice
                 return FALSE;
             }
 
-            $this->finv_amt += $fiit->fiit_amt;
+            $this->finv_amt   += $fiit->fiit_amt;
             $this->finv_total += $fiit->fiit_total;
-            $this->finv_vat += $fiit->fiit_vat;
+            $this->finv_vat   += $fiit->fiit_vat;
         }
 
-        $this->finv_basic_amt = Yii::app()->currency->convertToBase($this->finv_amt, $this->finv_fcrn_id, $this->finv_date);
+        $this->finv_basic_amt   = Yii::app()->currency->convertToBase($this->finv_amt,   $this->finv_fcrn_id, $this->finv_date);
         $this->finv_basic_total = Yii::app()->currency->convertToBase($this->finv_total, $this->finv_fcrn_id, $this->finv_date);
-        $this->finv_basic_vat = Yii::app()->currency->convertToBase($this->finv_vat, $this->finv_fcrn_id, $this->finv_date);
+        $this->finv_basic_vat   = Yii::app()->currency->convertToBase($this->finv_vat,   $this->finv_fcrn_id, $this->finv_date);
+        
         try {
-            if (!$this->save()) {
-                $this->addError('finv_id', 'Can not update finv record');
+            if (!parent::save()) {
+                $this->addError('finv_id', Yii::t('D2finvModule.model', 'Can not update invoice!'));
                 return FALSE;
             }
         } catch (Exception $e) {
             $this->addError('finv_id', $e->getMessage());
             return FALSE;
         }
-
+        
         return TRUE;
+
+    }
+    
+    public function duplicate($finv_id)
+    {
+        $sourceFinv = self::model()->findByPk($finv_id);
+        
+        if ($sourceFinv === null) {
+            return false;
+        }
+        
+        $this->attributes    = $sourceFinv->attributes;
+        $this->finv_reg_date = date('Y-m-d');
+        
+        $this->save();
+        
+        $criteria = new CDbCriteria;
+        $criteria->compare('t.fiit_finv_id', $finv_id);
+        $sourceFiits = FiitInvoiceItem::model()->findAll($criteria);
+        
+        foreach ($sourceFiits as $sourceFiit) {
+            $fiit = new FiitInvoiceItem;
+            $fiit->attributes   = $sourceFiit->attributes;
+            $fiit->fiit_finv_id = $this->finv_id;
+            
+            try {
+                if (!$fiit->saveWithoutRecalc()) {
+                    $this->addError('fiit_id', Yii::t('D2finvModule.model', 'Can not copy invoice item!'));
+                    return FALSE;
+                }
+            } catch (Exception $e) {
+                $this->addError('', $e->getMessage());
+                return FALSE;
+            }
+        }
+        
+        // save and recalc
+        return $this->save();
+        
+    }
+    
+    public function getTotals($column)
+    {
+        
+        $records = $this->search()->getData();
+        $total   = 0;
+        
+        foreach ($records as $record) {
+            $total += $record->$column;
+        }
+        
+        return number_format($total, 2, '.', '');
+        
+    }
+    
+    protected function beforeFind()
+    {
+        parent::beforeFind();
+        $criteria = new CDbCriteria;
+        $criteria->compare('t.finv_sys_ccmp_id', Yii::app()->sysCompany->getActiveCompany());
+        
+        /**
+         * filter date to from
+         */
+        if (!empty($this->finv_date_range)) {
+            $criteria->AddCondition("t.finv_date >= '".substr($this->finv_date_range,0,10)."'");
+            $criteria->AddCondition("t.finv_date <= '".substr($this->finv_date_range,-10)."'");
+        }
+        
+        $this->dbCriteria->mergeWith($criteria);   
+    }
+    
+    protected function beforeDelete()
+    {
+        
+        $criteria = new CDbCriteria;
+        $criteria->compare('t.fiit_finv_id', $this->finv_id);
+        $fiits = FiitInvoiceItem::model()->findAll($criteria);
+        
+        foreach ($fiits as $fiit) {
+            $fiit->delete();
+        }
+        
+        return parent::beforeDelete();
+        
     }
 
 }
