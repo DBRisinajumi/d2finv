6a9d89b798d48382393fdffb412d6983e90fe953
ivarsju <ivars.ju@gmail.com>
2014-06-05 03:08:53 +0300

    column changes, added datepicker (not working yet) and other changes

--- views/finvInvoice/admin.php
+++ views/finvInvoice/admin.php
@@ -1,4 +1,11 @@
 <?php
+
+Yii::app()->clientScript->registerScript('re-install-date-picker', "
+function reinstallDatePicker(id, data) {
+    filter_FinvInvoice_finv_date_range_init();
+   }
+");
+
 $this->setPageTitle(
     Yii::t('D2finvModule.model', 'Finv Invoices')
     . ' - '
@@ -47,8 +54,9 @@ $this->widget('TbGridView',
             'class' => 'TbPager',
             'displayFirstAndLast' => true,
         ),
+        'afterAjaxUpdate' => 'reinstallDatePicker',
         'columns' => array(
-            array(
+            /*array(
                 'class' => 'CLinkColumn',
                 'header' => '',
                 'labelExpression' => '$data->itemLabel',
@@ -62,7 +70,7 @@ $this->widget('TbGridView',
                     'url' => $this->createUrl('/d2finv/finvInvoice/editableSaver'),
                     //'placement' => 'right',
                 )
-            ),
+            ),*/
             array(
                 //varchar(10)
                 'class' => 'editable.EditableColumn',
@@ -81,7 +89,7 @@ $this->widget('TbGridView',
                     //'placement' => 'right',
                 )
             ),
-            array(
+            /*array(
                 'class' => 'editable.EditableColumn',
                 'name' => 'finv_sys_ccmp_id',
                 'editable' => array(
@@ -90,10 +98,12 @@ $this->widget('TbGridView',
                     'source' => CHtml::listData(CcmpCompany::model()->findAll(array('limit' => 1000)), 'ccmp_id', 'itemLabel'),                        
                     //'placement' => 'right',
                 )
-            ),
+            ),*/
             array(
                 'class' => 'editable.EditableColumn',
                 'name' => 'finv_ccmp_id',
+                'value' => 'CHtml::value($data, \'finvCcmp.itemLabel\')',
+                'filter' => CHtml::listData(CcmpCompany::model()->findAll(array('limit' => 1000,'order'=>'ccmp_name')), 'ccmp_id', 'itemLabel'),
                 'editable' => array(
                     'type' => 'select',
                     'url' => $this->createUrl('/d2finv/finvInvoice/editableSaver'),
@@ -101,7 +111,7 @@ $this->widget('TbGridView',
                     //'placement' => 'right',
                 )
             ),
-            array(
+            /*array(
                 'class' => 'editable.EditableColumn',
                 'name' => 'finv_reg_date',
                 'editable' => array(
@@ -109,7 +119,7 @@ $this->widget('TbGridView',
                     'url' => $this->createUrl('/d2finv/finvInvoice/editableSaver'),
                     //'placement' => 'right',
                 )
-            ),
+            ),*/
             array(
                 'class' => 'editable.EditableColumn',
                 'name' => 'finv_date',
@@ -117,9 +127,20 @@ $this->widget('TbGridView',
                     'type' => 'date',
                     'url' => $this->createUrl('/d2finv/finvInvoice/editableSaver'),
                     //'placement' => 'right',
+                ),
+                'filter' => $this->widget(
+                    'application.widgets.TbFilterDateRangePicker', 
+                    array(
+                       'model' => $model,
+                       'attribute' => 'finv_date_range',
+                       'options' => array(
+                           'ranges' => array('today','yesterday','this_week','last_week','this_month','last_month','this_year'),
+                       )
+                    ),
+                    TRUE 
                 )
             ),
-            array(
+            /*array(
                 'class' => 'editable.EditableColumn',
                 'name' => 'finv_budget_date',
                 'editable' => array(
@@ -128,7 +149,7 @@ $this->widget('TbGridView',
                     //'placement' => 'right',
                 )
             ),
-            /*
+            
             array(
                 'class' => 'editable.EditableColumn',
                 'name' => 'finv_due_date',
@@ -156,35 +177,44 @@ $this->widget('TbGridView',
                     'source' => CHtml::listData(FcrnCurrency::model()->findAll(array('limit' => 1000)), 'fcrn_id', 'itemLabel'),                        
                     //'placement' => 'right',
                 )
-            ),
+            ),*/
             array(
                 //decimal(10,2)
-                'class' => 'editable.EditableColumn',
+                'htmlOptions' => array('class' => 'numeric-column'),
+                //'class' => 'editable.EditableColumn',
                 'name' => 'finv_amt',
-                'editable' => array(
-                    'url' => $this->createUrl('/d2finv/finvInvoice/editableSaver'),
-                    //'placement' => 'right',
-                )
+                //'editable' => array(
+                //    'url' => $this->createUrl('/d2finv/finvInvoice/editableSaver'),
+                //    //'placement' => 'right',
+                //)
+                'footer' => $model->getTotals('finv_amt'),
+                'footerHtmlOptions' => array('class' => 'total-row numeric-column'),
             ),
             array(
                 //decimal(10,2)
-                'class' => 'editable.EditableColumn',
+                'htmlOptions' => array('class' => 'numeric-column'),
+                //'class' => 'editable.EditableColumn',
                 'name' => 'finv_vat',
-                'editable' => array(
-                    'url' => $this->createUrl('/d2finv/finvInvoice/editableSaver'),
-                    //'placement' => 'right',
-                )
+                //'editable' => array(
+                //    'url' => $this->createUrl('/d2finv/finvInvoice/editableSaver'),
+                //    //'placement' => 'right',
+                //)
+                'footer' => $model->getTotals('finv_vat'),
+                'footerHtmlOptions' => array('class' => 'total-row numeric-column'),
             ),
             array(
                 //decimal(10,2)
-                'class' => 'editable.EditableColumn',
+                'htmlOptions' => array('class' => 'numeric-column'),
+                //'class' => 'editable.EditableColumn',
                 'name' => 'finv_total',
-                'editable' => array(
-                    'url' => $this->createUrl('/d2finv/finvInvoice/editableSaver'),
-                    //'placement' => 'right',
-                )
+                //'editable' => array(
+                //    'url' => $this->createUrl('/d2finv/finvInvoice/editableSaver'),
+                //    //'placement' => 'right',
+                //)
+                'footer' => $model->getTotals('finv_total'),
+                'footerHtmlOptions' => array('class' => 'total-row numeric-column'),
             ),
-            array(
+            /*array(
                 'class' => 'editable.EditableColumn',
                 'name' => 'finv_basic_fcrn_id',
                 'editable' => array(
@@ -229,10 +259,12 @@ $this->widget('TbGridView',
                     'url' => $this->createUrl('/d2finv/finvInvoice/editableSaver'),
                     //'placement' => 'right',
                 )
-            ),
+            ),*/
             array(
                 'class' => 'editable.EditableColumn',
                 'name' => 'finv_stst_id',
+                'value' => 'CHtml::value($data, \'finvStst.itemLabel\')',
+                'filter' => CHtml::listData(StstState::model()->findAll(array('limit' => 1000)), 'stst_id', 'itemLabel'),
                 'editable' => array(
                     'type' => 'select',
                     'url' => $this->createUrl('/d2finv/finvInvoice/editableSaver'),
@@ -251,7 +283,7 @@ $this->widget('TbGridView',
                     ),
                    'filter' => $model->getEnumFieldLabels('finv_paid'),
                 ),
-            array(
+            /*array(
                     'class' => 'editable.EditableColumn',
                     'name' => 'finv_ref',
                     'editable' => array(
@@ -286,14 +318,23 @@ $this->widget('TbGridView',
 
             array(
                 'class' => 'TbButtonColumn',
+                'template'=>'{view} {update} {delete} {copy}',
                 'buttons' => array(
-                    'view' => array('visible' => 'Yii::app()->user->checkAccess("D2finv.FinvInvoice.View")'),
-                    'update' => array('visible' => 'FALSE'),
-                    'delete' => array('visible' => 'Yii::app()->user->checkAccess("D2finv.FinvInvoice.Delete")'),
+                    'view' => array('visible' => '!Yii::app()->user->checkAccess("InvoiceEdit")'),
+                    'update' => array('visible' => 'Yii::app()->user->checkAccess("InvoiceEdit")'),
+                    'delete' => array('visible' => 'Yii::app()->user->checkAccess("InvoiceEdit")'),
+                    'copy' => array(
+                        'label'=>'Copy',
+                        'icon'=>'copy',
+                        'url'=>'Yii::app()->controller->createUrl("copy", array("finv_id" => $data->finv_id))',
+                        'visible' => 'Yii::app()->user->checkAccess("InvoiceEdit")'
+                    ),
                 ),
                 'viewButtonUrl' => 'Yii::app()->controller->createUrl("view", array("finv_id" => $data->finv_id))',
+                'updateButtonUrl' => 'Yii::app()->controller->createUrl("view", array("finv_id" => $data->finv_id))',
                 'deleteButtonUrl' => 'Yii::app()->controller->createUrl("delete", array("finv_id" => $data->finv_id))',
-                'viewButtonOptions'=>array('data-toggle'=>'tooltip'),   
+                'viewButtonOptions'=>array('data-toggle'=>'tooltip'),
+                'updateButtonOptions'=>array('data-toggle'=>'tooltip'),   
                 'deleteButtonOptions'=>array('data-toggle'=>'tooltip'),   
             ),
         )
