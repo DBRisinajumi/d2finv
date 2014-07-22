4c28bd66fb488a76c415af03f3a83977b410322b
ivarsju <ivars.ju@gmail.com>
2014-07-22 05:13:19 +0300

    added wiki title

--- views/finvInvoice/view.php
+++ views/finvInvoice/view.php
@@ -6,6 +6,13 @@
         . ': '   
         . $model->getItemLabel()            
 );    
+    
+    $this->setWikiTitle(
+        Yii::t('D2finvModule.model', 'Finv Invoice')      
+        . ' - '
+        . Yii::t('D2finvModule.crud_static', 'View')
+);    
+    
 //$this->breadcrumbs[Yii::t('D2finvModule.model','Finv Invoices')] = array('admin');
 //$this->breadcrumbs[$model->{$model->tableSchema->primaryKey}] = array('view','id' => $model->{$model->tableSchema->primaryKey});
 //$this->breadcrumbs[] = Yii::t('D2finvModule.crud_static', 'View');
